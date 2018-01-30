28675f46cdaccb6e56d13671920e2de07990f274
Devin Huang <devin.huangchenda@gmail.com>
2018-01-29 22:30:09 -0500

    Number Drills.

---
+++ Project_JS/NumberDrills.js
@@ -0,0 +1,18 @@
+function computeArea(width, height) {
+    return width * height;
+}
+
+
+function convertCelsiusToFahrenheit(celsius) {
+    return celsius * 1.8 + 32;
+}
+
+
+function convertFahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) / 1.8;
+}
+
+
+function isDivisible(divisee, divisor) {
+    return divisee % divisor === 0;
+}
--- Project_JS/StringDrills.js
+++ Project_JS/StringDrills.js
@@ -1,18 +1,16 @@
 'use strict'
 
-function wisePerson(wiseType, whatToSay){
+function wisePerson(wiseType, whatToSay) {
     return 'A wise ' + wiseType + ' once said: "' + whatToSay + '".';
 }
 
 
-
-function shouter(whatToShout){
+function shouter(whatToShout) {
     let shoutMessage = whatToShout + '!!!';
     return shoutMessage.toUpperCase();
 }
 
 
-
-function textNormalizer(text){
+function textNormalizer(text) {
     return text.trim().toLowerCase();
 }
